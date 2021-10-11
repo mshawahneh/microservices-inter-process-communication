6602819fea600b733cdc92e29c8b7f678cf65b5b
mshawahneh <mohd.shawahneh@gmail.com>
2021-10-11 10:47:52 +0300

    Enable eureka client

--- experiment-micro-service6/src/main/java/org/bzu/exp/ExperimentMicroService6Application.java
+++ experiment-micro-service6/src/main/java/org/bzu/exp/ExperimentMicroService6Application.java
@@ -2,8 +2,10 @@ package org.bzu.exp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ExperimentMicroService6Application {
 
 	public static void main(String[] args) {

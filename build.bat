d6e66e3e217539ce02d4de2cd5f53d27f236ba51
mshawahneh <mohd.shawahneh@gmail.com>
2021-10-13 13:02:41 +0300

    add docker builds

--- build.bat
+++ build.bat
@@ -1,24 +1,15 @@
 @ECHO OFF
-
-SET baseDir=C:\future\Thesis\git\microservices-inter-process-communication\experiment-micro-service
-
-FOR %%A in (1 2 3 4 5 6 7) DO (
-
-cd %baseDir%%%A
-mvn clean install -DskipTests
-
-ECHO Done with service%%A
-
-)
-
 cd C:\future\Thesis\git\microservices-inter-process-communication\experiment-api-gateway
-mvn clean install -DskipTests
-
+call mvn clean install -DskipTests
 ECHO Done with Gateway
-
 cd C:\future\Thesis\git\microservices-inter-process-communication\experiment-discovery-service
-mvn clean install -DskipTests
-
+call mvn clean install -DskipTests
 ECHO Done with Discovery 
+SET baseDir=C:\future\Thesis\git\microservices-inter-process-communication\experiment-micro-service
+FOR %%A in (1 2 3 4 5 6 7) DO (
+cd %baseDir%%%A
+call mvn clean install -DskipTests
+ECHO Done with service%%A
+)
 echo 'Now Done All services Done
 pause
